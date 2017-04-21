7c9168b515e59df7d94b414abf22a1d5077b14da
wrighp <wrightcoder@gmail.com>
2017-04-20 21:34:54 -0400

    Test Scene

---
+++ Assets/DepthIntersectionCamera.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DepthIntersectionCamera : MonoBehaviour {
+	public int resolution = 128;
+	private Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+		SwapRenderTexture (MakeRenderTexture ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	/// <summary>
+	/// Makes the render texture.
+	/// </summary>
+	/// <returns>The render texture.</returns>
+	/// <param name="scaleTexture">If set to <c>true</c> scale texture to non power of 2 values based on camera x and yscale (results in smaller render texture).</param>
+	RenderTexture MakeRenderTexture(bool scaleTexture = false){
+		int xPixels, yPixels;
+		if (scaleTexture) {
+			float xRatio = transform.localScale.x / transform.localScale.y;
+			float xScale = transform.localScale.x;
+			float yScale = transform.localScale.y;
+
+			if (xScale > yScale) {
+				xPixels = resolution;
+				yPixels = (int)(1f / xRatio * resolution);
+			} else { //yScale >= xScale
+				xPixels = (int)(xRatio * resolution);
+				yPixels = resolution;				
+			}
+		} else {
+			xPixels = resolution;
+			yPixels = resolution;
+		}
+		RenderTexture rt = new RenderTexture (xPixels, yPixels, 24, RenderTextureFormat.Depth, RenderTextureReadWrite.Linear);
+		rt.wrapMode = TextureWrapMode.Clamp;
+		rt.filterMode = FilterMode.Trilinear;
+		rt.Create ();
+		return rt;
+	}
+		
+	void SwapRenderTexture(RenderTexture swapIn){
+		if(cam.targetTexture != null){
+			cam.targetTexture.Release ();
+		}
+		cam.targetTexture = swapIn;
+
+	}
+	void OnDestroy(){
+		SwapRenderTexture (null);
+	}
+
+}
---
+++ Assets/SetRenderTexture.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRenderTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<MeshRenderer> ().material.mainTexture = Camera.main.targetTexture;
+	}
+}
